8a0fb1153a285f4ae96a8b860874ee5ac36cbc71
Vyshalee-Sen <128276091+Vyshalee-Sen@users.noreply.github.com>
2025-04-23 19:06:10 +0530

    Update genai_chatbot.py

--- genai_chatbot.py
+++ genai_chatbot.py
@@ -1,6 +1,12 @@
 # app.py
 import streamlit as st
 from Rag_llm import load_model, get_response  # You need to define these based on your notebook
+import re
+import numpy as np
+from sentence_transformers import SentenceTransformer
+import faiss
+from transformers import pipeline, AutoModelForCausalLM, AutoTokenizer
+from huggingface_hub import login
 
 
 # Load model and retriever (do it only once)
